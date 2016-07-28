2dd1882379e3a5794bb3accd10cba888fcc6b86b
BurpSuite <BurpSuite@iCloud.com>
2016-07-28 18:19:19 +0800

    3.6
    3.6

--- Proxy/GET/Potatso.php
+++ Proxy/GET/Potatso.php
@@ -3,6 +3,36 @@
 header("cache-control:no-cache,must-revalidate");//No-Cache
 header("Content-Type:text/html;charset=UTF-8");//UTF-8
 //-------------通用-------------//
+if( isset($_GET['Config1']) ){//判断
+$Config1 = $_GET['Config1'];//配置
+}else {
+$Config1 = "ss://salsa20:12345@10.0.0.0:18650";//配置
+}
+if( isset($_GET['Config2']) ){//判断
+$Config2 = $_GET['Config2'];//配置
+}else {
+$Config2 = "ss://chacha20:12345@10.0.0.0:18650";//配置
+}
+if( isset($_GET['Config3']) ){//判断
+$Config3 = $_GET['Config3'];//配置
+}else {
+$Config3 = "ss://aes-256-cfb:12345@10.0.0.0:18650";//配置
+}
+if( isset($_GET['Flag1']) ){//判断
+$Flag1 = $_GET['Flag1'];    //配置
+}else {
+$Flag1 = "JP";//配置
+}
+if( isset($_GET['Flag2']) ){//判断
+$Flag2 = $_GET['Flag2'];    //配置
+}else {
+$Flag2 = "SG";//配置
+}
+if( isset($_GET['Flag3']) ){//判断
+$Flag3 = $_GET['Flag3'];    //配置
+}else {
+$Flag3 = "KR";//配置
+}
 $NAME = "UPlus";            //名称
 //-------------文件-------------//
 $DefaultFile = "http://7xpphx.com1.z0.glb.clouddn.com/Proxy/File/Default.txt";
@@ -33,16 +63,20 @@ header('Content-type: application/octet-stream; charset=utf8');//下载动作
 header("Accept-Ranges: bytes");
 header('Content-Disposition: attachment; filename='.$File);//名称
 //--------------配置------------//
-echo"proxies:\r\n";
-echo"- name: ss1\r\n";
-echo"  type: SS\r\n";
-echo"  host: 10.0.0.0\r\n";
-echo"  port: 443\r\n";
-echo"  encryption: rc4-md5\r\n";
-echo"  password: 12345\r\n";
-echo"#\r\n";
-echo"- name: linode\r\n";
-echo"  uri: ss://salsa20:12345@10.0.0.0:443\r\n";
+//echo"proxies:\r\n";
+//echo"- name: ss1\r\n";
+//echo"  type: SS\r\n";
+//echo"  host: 10.0.0.0\r\n";
+//echo"  port: 443\r\n";
+//echo"  encryption: rc4-md5\r\n";
+//echo"  password: 12345\r\n";
+//echo"#\r\n";
+echo"- name: $Flag1\r\n";
+echo"  uri: $Config1\r\n";
+echo"- name: $Flag2\r\n";
+echo"  uri: $Config2\r\n";
+echo"- name: $Flag3\r\n";
+echo"  uri: $Config3\r\n";
 echo"#\r\n";
 echo"ruleSets:\r\n";
 echo"# \r\n";
