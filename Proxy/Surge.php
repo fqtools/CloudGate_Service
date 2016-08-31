d0d4c75d80023c2f402bee4330964f2d289fe219
BurpSuite <BurpSuite@iCloud.com>
2016-08-31 10:14:25 +0800

    4.0
    4.0

--- Proxy/Surge.php
+++ Proxy/Surge.php
@@ -3,7 +3,7 @@
 header("cache-control:no-cache,must-revalidate");
 header("Content-Type:text/html;charset=UTF-8");
 header("Accept-Ranges: bytes");
-header('Content-Disposition: attachment; filename='.$File);
+header('Content-Disposition: attachment; filename='.$NAME.Conf);
 //-------------通用-------------//
 $File = "Surge.Conf";   //文件名称
 $NAME = "UPlus";            //名称
