f6c4846b92010501ef1b2b7cf3a25b70e1869f96
BurpSuite <BurpSuite@iCloud.com>
2016-11-09 00:58:58 +0800

    Fix Bug

--- Proxy/Hosts.php
+++ Proxy/Hosts.php
@@ -5,17 +5,20 @@ USERAGENT Location
 //Header
 header("cache-control:no-cache,must-revalidate");
 header("Content-Type:text/html;charset=UTF-8");
+
+//GET
+if( isset($_GET['Fix']) ){$Fix = $_GET['Fix'];}else {$Fix = "false";}
 //USERAGENT
 $USERAGENT = $_SERVER['HTTP_USER_AGENT'];
 //Surge
 if(strstr($USERAGENT,"Surge")){
-header("Location:".'http://UPlus7.Win/Hosts/Surge-Hosts.php');
+header("Location:".'http://UPlus7.Win/Hosts/Surge-Hosts.php?Fix=$Fix');
 }
 //Shadowrocket
 elseif(strstr($USERAGENT,"Shadowrocket")){
-header("Location:".'http://UPlus7.Win/Hosts/Shadowrocket.php');
+header("Location:".'http://UPlus7.Win/Hosts/Shadowrocket.php?Fix=$Fix');
 }
 //Other
 else{
-header("Location:".'http://UPlus7.Win/Hosts/Surge-Hosts.php');
+header("Location:".'http://UPlus7.Win/Hosts/Surge-Hosts.php?Fix=$Fix');
 }
