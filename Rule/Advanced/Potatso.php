4a8103fd73f49982ec4b224cabb453491b470133
BurpSuite <BurpSuite@iCloud.com>
2016-11-20 13:52:35 +0800

    Fix Hosts IP / Fix Potatso Advanced Rule Format

--- Rule/Advanced/Potatso.php
+++ Rule/Advanced/Potatso.php
@@ -16,11 +16,16 @@ if( $Apple=="true" ){$GETApple="Proxy";}elseif ($Apple=="false"){$GETApple="DIRE
 if( isset($_GET['Group']) ){$Group = $_GET['Group'];}else {$Group = "1";}
 //if( isset($_GET['DNS1']) ){$DNS1 = $_GET['DNS1'];}else {$DNS1 = "8.8.8.8";}
 //if( isset($_GET['DNS2']) ){$DNS2 = $_GET['DNS2'];}else {$DNS2 = "8.8.4.4";}
-if( isset($_GET['Config1']) ){$Config1 = $_GET['Config1'];}else {$Config1 = "127.0.0.1,80,aes-256-cfb,Password";}
-if( isset($_GET['Config2']) ){$Config2 = $_GET['Config2'];}else {$Config2 = "127.0.0.1,80,aes-256-cfb,Password";}
-if( isset($_GET['Config3']) ){$Config3 = $_GET['Config3'];}else {$Config3 = "127.0.0.1,80,aes-256-cfb,Password";}
-if( isset($_GET['Config4']) ){$Config4 = $_GET['Config4'];}else {$Config4 = "127.0.0.1,80,aes-256-cfb,Password";}
-if( isset($_GET['Config5']) ){$Config5 = $_GET['Config5'];}else {$Config5 = "127.0.0.1,80,aes-256-cfb,Password";}
+if( isset($_GET['Config1']) ){$Config1 = $_GET['Config1'];}else {$Config1 = "SS,127.0.0.1,80,aes-256-cfb,Password";}
+$Config1Explode = (explode(",",$Config1));
+if( isset($_GET['Config2']) ){$Config2 = $_GET['Config2'];}else {$Config2 = "SS,127.0.0.1,80,aes-256-cfb,Password";}
+$Config2Explode = (explode(",",$Config2));
+if( isset($_GET['Config3']) ){$Config3 = $_GET['Config3'];}else {$Config3 = "SS,127.0.0.1,80,aes-256-cfb,Password";}
+$Config3Explode = (explode(",",$Config3));
+if( isset($_GET['Config4']) ){$Config4 = $_GET['Config4'];}else {$Config4 = "SS,127.0.0.1,80,aes-256-cfb,Password";}
+$Config4Explode = (explode(",",$Config4));
+if( isset($_GET['Config5']) ){$Config5 = $_GET['Config5'];}else {$Config5 = "SS,127.0.0.1,80,aes-256-cfb,Password";}
+$Config5Explode = (explode(",",$Config5));
 if( isset($_GET['Flag1']) ){$Flag1 = $_GET['Flag1'];  }else {$Flag1 = "NONE1";}
 if( isset($_GET['Flag2']) ){$Flag2 = $_GET['Flag2'];  }else {$Flag2 = "NONE2";}
 if( isset($_GET['Flag3']) ){$Flag3 = $_GET['Flag3'];  }else {$Flag3 = "NONE3";}
@@ -54,45 +59,113 @@ echo"proxies:\r\n";
 echo"#\r\n";
 if ($Group<"2"){
 echo"- name: $Flag1\r\n";
-echo"  uri: $Config1\r\n";}
+echo"  type: ".$Config1Explode[0]."\r\n";
+echo"  host: ".$Config1Explode[1]."\r\n";
+echo"  port: ".$Config1Explode[2]."\r\n";
+echo"  encryption: ".$Config1Explode[3]."\r\n";
+echo"  password: ".$Config1Explode[4]."\r\n";}
 elseif ($Group<"3"){
 echo"- name: $Flag1\r\n";
-echo"  uri: $Config1\r\n";
+echo"  type: ".$Config1Explode[0]."\r\n";
+echo"  host: ".$Config1Explode[1]."\r\n";
+echo"  port: ".$Config1Explode[2]."\r\n";
+echo"  encryption: ".$Config1Explode[3]."\r\n";
+echo"  password: ".$Config1Explode[4]."\r\n";
 echo"- name: $Flag2\r\n";
-echo"  uri: $Config2\r\n";}
+echo"- type: ".$Config2Explode[0]."\r\n";
+echo"  host: ".$Config2Explode[1]."\r\n";
+echo"  port: ".$Config2Explode[2]."\r\n";
+echo"  encryption: ".$Config2Explode[3]."\r\n";
+echo"  password: ".$Config2Explode[4]."\r\n";}
 elseif ($Group<"4"){
 echo"- name: $Flag1\r\n";
-echo"  uri: $Config1\r\n";
+echo"  type: ".$Config1Explode[0]."\r\n";
+echo"  host: ".$Config1Explode[1]."\r\n";
+echo"  port: ".$Config1Explode[2]."\r\n";
+echo"  encryption: ".$Config1Explode[3]."\r\n";
+echo"  password: ".$Config1Explode[4]."\r\n";
 echo"- name: $Flag2\r\n";
-echo"  uri: $Config2\r\n";
+echo"- type: ".$Config2Explode[0]."\r\n";
+echo"  host: ".$Config2Explode[1]."\r\n";
+echo"  port: ".$Config2Explode[2]."\r\n";
+echo"  encryption: ".$Config2Explode[3]."\r\n";
+echo"  password: ".$Config2Explode[4]."\r\n";
 echo"- name: $Flag3\r\n";
-echo"  uri: $Config3\r\n";}
+echo"  type: ".$Config3Explode[0]."\r\n";
+echo"  host: ".$Config3Explode[1]."\r\n";
+echo"  port: ".$Config3Explode[2]."\r\n";
+echo"  encryption: ".$Config3Explode[3]."\r\n";
+echo"  password: ".$Config3Explode[4]."\r\n";}
 elseif ($Group<"5"){
 echo"- name: $Flag1\r\n";
-echo"  uri: $Config1\r\n";
+echo"  type: ".$Config1Explode[0]."\r\n";
+echo"  host: ".$Config1Explode[1]."\r\n";
+echo"  port: ".$Config1Explode[2]."\r\n";
+echo"  encryption: ".$Config1Explode[3]."\r\n";
+echo"  password: ".$Config1Explode[4]."\r\n";
 echo"- name: $Flag2\r\n";
-echo"  uri: $Config2\r\n";
+echo"  type: ".$Config2Explode[0]."\r\n";
+echo"  host: ".$Config2Explode[1]."\r\n";
+echo"  port: ".$Config2Explode[2]."\r\n";
+echo"  encryption: ".$Config2Explode[3]."\r\n";
+echo"  password: ".$Config2Explode[4]."\r\n";
 echo"- name: $Flag3\r\n";
-echo"  uri: $Config3\r\n";
+echo"  type: ".$Config3Explode[0]."\r\n";
+echo"  host: ".$Config3Explode[1]."\r\n";
+echo"  port: ".$Config3Explode[2]."\r\n";
+echo"  encryption: ".$Config3Explode[3]."\r\n";
+echo"  password: ".$Config3Explode[4]."\r\n";
 echo"- name: $Flag4\r\n";
-echo"  uri: $Config4\r\n";}
+echo"  type: ".$Config4Explode[0]."\r\n";
+echo"  host: ".$Config4Explode[1]."\r\n";
+echo"  port: ".$Config4Explode[2]."\r\n";
+echo"  encryption: ".$Config4Explode[3]."\r\n";
+echo"  password: ".$Config4Explode[4]."\r\n";}
 elseif ($Group<"6"){
 echo"- name: $Flag1\r\n";
-echo"  uri: $Config1\r\n";
+echo"  type: ".$Config1Explode[0]."\r\n";
+echo"  host: ".$Config1Explode[1]."\r\n";
+echo"  port: ".$Config1Explode[2]."\r\n";
+echo"  encryption: ".$Config1Explode[3]."\r\n";
+echo"  password: ".$Config1Explode[4]."\r\n";
 echo"- name: $Flag2\r\n";
-echo"  uri: $Config2\r\n";
+echo"  type: ".$Config2Explode[0]."\r\n";
+echo"  host: ".$Config2Explode[1]."\r\n";
+echo"  port: ".$Config2Explode[2]."\r\n";
+echo"  encryption: ".$Config2Explode[3]."\r\n";
+echo"  password: ".$Config2Explode[4]."\r\n";
 echo"- name: $Flag3\r\n";
-echo"  uri: $Config3\r\n";
+echo"  type: ".$Config3Explode[0]."\r\n";
+echo"  host: ".$Config3Explode[1]."\r\n";
+echo"  port: ".$Config3Explode[2]."\r\n";
+echo"  encryption: ".$Config3Explode[3]."\r\n";
+echo"  password: ".$Config3Explode[4]."\r\n";
 echo"- name: $Flag4\r\n";
-echo"  uri: $Config4\r\n";
+echo"  type: ".$Config4Explode[0]."\r\n";
+echo"  host: ".$Config4Explode[1]."\r\n";
+echo"  port: ".$Config4Explode[2]."\r\n";
+echo"  encryption: ".$Config4Explode[3]."\r\n";
+echo"  password: ".$Config4Explode[4]."\r\n";
 echo"- name: $Flag5\r\n";
-echo"  uri: $Config5\r\n";}
+echo"  type: ".$Config5Explode[0]."\r\n";
+echo"  host: ".$Config5Explode[1]."\r\n";
+echo"  port: ".$Config5Explode[2]."\r\n";
+echo"  encryption: ".$Config5Explode[3]."\r\n";
+echo"  password: ".$Config5Explode[4]."\r\n";}
 elseif ($Group>"6"){
 echo"- name: $Flag1\r\n";
-echo"  uri: $Config1\r\n";}
+echo"  type: ".$Config1Explode[0]."\r\n";
+echo"  host: ".$Config1Explode[1]."\r\n";
+echo"  port: ".$Config1Explode[2]."\r\n";
+echo"  encryption: ".$Config1Explode[3]."\r\n";
+echo"  password: ".$Config1Explode[4]."\r\n";}
 else {
 echo"- name: $Flag1\r\n";
-echo"  uri: $Config1\r\n";}
+echo"  type: ".$Config1Explode[0]."\r\n";
+echo"  host: ".$Config1Explode[1]."\r\n";
+echo"  port: ".$Config1Explode[2]."\r\n";
+echo"  encryption: ".$Config1Explode[3]."\r\n";
+echo"  password: ".$Config1Explode[4]."\r\n";}
 echo"#\r\n";
 echo"ruleSets:\r\n";
 echo"# \r\n";
